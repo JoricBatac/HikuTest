4420dc4b74370b4fb3c53c0bc2e5fbbad904f7b3
makorino <llorca.mfs@gmail.com>
2021-05-23 02:35:19 +0800

    connected to online db

--- models/db.js
+++ models/db.js
@@ -8,7 +8,9 @@ const ForumDiscussion = require('./ForumDiscussionModel.js');
 //const dotenv = require('dotenv');
 //dotenv.config();
 //const url = process.env.DB_URL;//'mongodb://localhost:27017/hiku-rework';
-const url = 'mongodb://localhost:27017/hiku-rework';
+//const url = 'mongodb://localhost:27017/hiku-rework';
+
+const url = "mongodb+srv://hiku-dev:amamiyasora@hiku-onlinedb-cluster.ykzrc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const options = {
   useUnifiedTopology: true,
